07c04f9b6139b07b91017e311cb054c4a653ec38
ybakesweetcandy <khizarkhizarr@gmail.com>
2025-02-09 02:54:40 +0000

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "ContractSignage.co.uk";
-export const SITE_DESCRIPTION = Your Trusted UK Partner for Signage Dropshipping";
+export const SITE_DESCRIPTION = "Your Trusted UK Partner for Signage Dropshipping";
